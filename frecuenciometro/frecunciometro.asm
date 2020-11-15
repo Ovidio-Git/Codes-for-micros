20c272810792f66c7199ad27d7186affba3af5d4
Ovidio Andrade <oviidio.andrade@gmail.com>
2020-11-15 01:59:37 -0500

    program finish

--- frecuenciometro/frecunciometro.asm
+++ frecuenciometro/frecunciometro.asm
@@ -8,25 +8,23 @@ _interrupt:
 	MOVWF      ___savePCLATH+0
 	CLRF       PCLATH+0
 
-;frecunciometro.c,25 :: 		void interrupt(void)  //ist
-;frecunciometro.c,27 :: 		INTCON.GIE = 0; //Deshabilito todas las interrupciones
+;frecunciometro.c,21 :: 		void interrupt(void)
+;frecunciometro.c,23 :: 		INTCON.GIE = 0;       // Se deshabilitan todas las interrupciones
 	BCF        INTCON+0, 7
-;frecunciometro.c,28 :: 		if(INTCON.T0IF == 1)
+;frecunciometro.c,24 :: 		if(INTCON.T0IF == 1 ) // se chequea si el timer 1 fue el causante
 	BTFSS      INTCON+0, 2
 	GOTO       L_interrupt0
-;frecunciometro.c,30 :: 		INTCON.T0IF = 0; //Bajar la bandera
+;frecunciometro.c,26 :: 		INTCON.T0IF = 0;   //se baja la bandera del timer 1
 	BCF        INTCON+0, 2
-;frecunciometro.c,31 :: 		PORTD=~PORTD;
-	COMF       PORTD+0, 1
-;frecunciometro.c,32 :: 		ctrl[0]++;
+;frecunciometro.c,27 :: 		ctrl++;            // se lleva control del numero de interrupciones
 	INCF       _ctrl+0, 1
-;frecunciometro.c,34 :: 		}
+;frecunciometro.c,28 :: 		}
 L_interrupt0:
-;frecunciometro.c,35 :: 		INTCON.GIE = 1;//Habilito todas las interrupciones
+;frecunciometro.c,29 :: 		INTCON.GIE = 1;       //se Habilitan todas las interrupciones
 	BSF        INTCON+0, 7
-;frecunciometro.c,36 :: 		}
+;frecunciometro.c,30 :: 		}
 L_end_interrupt:
-L__interrupt12:
+L__interrupt6:
 	MOVF       ___savePCLATH+0, 0
 	MOVWF      PCLATH+0
 	SWAPF      ___saveSTATUS+0, 0
@@ -38,41 +36,40 @@ L__interrupt12:
 
 _main:
 
-;frecunciometro.c,39 :: 		int main(void)
-;frecunciometro.c,41 :: 		TRISB=0x00;
-	CLRF       TRISB+0
-;frecunciometro.c,42 :: 		TRISA  = 0xFF;
-	MOVLW      255
-	MOVWF      TRISA+0
-;frecunciometro.c,43 :: 		PORTA  = 0x00;
-	CLRF       PORTA+0
-;frecunciometro.c,44 :: 		ANSELH = 0x00;
-	CLRF       ANSELH+0
-;frecunciometro.c,45 :: 		TRISD  = 0x00;
-	CLRF       TRISD+0
-;frecunciometro.c,46 :: 		PORTD  = 0x00;
-	CLRF       PORTD+0
-;frecunciometro.c,47 :: 		OPTION_REG = 0x27;
-	MOVLW      39
+;frecunciometro.c,33 :: 		int main(void)
+;frecunciometro.c,36 :: 		TRISC = 0x00;
+	CLRF       TRISC+0
+;frecunciometro.c,37 :: 		PORTC = 0x00;         //se inicializa el puerto c e 0
+	CLRF       PORTC+0
+;frecunciometro.c,38 :: 		OPTION_REG = 0x24;    //configurando timer0 como temporizador con un preescalador de 1:32
+	MOVLW      36
 	MOVWF      OPTION_REG+0
-;frecunciometro.c,48 :: 		INTCON =   0xA0;
-	MOVLW      160
-	MOVWF      INTCON+0
-;frecunciometro.c,49 :: 		TMR0   = 0x00;
+;frecunciometro.c,39 :: 		T1CON  = 0x03;           //configurando timer1 como contador
+	MOVLW      3
+	MOVWF      T1CON+0
+;frecunciometro.c,40 :: 		TMR0   = 0;           //inicializando timer0 en 0 para segurar que este limpio
 	CLRF       TMR0+0
-;frecunciometro.c,50 :: 		OPTION_REG.T0CS = 0; //HABILITO EL TIMER
+;frecunciometro.c,41 :: 		OPTION_REG.T0CS = 0;  //se habilita el timer 0
 	BCF        OPTION_REG+0, 5
-;frecunciometro.c,52 :: 		Lcd_Init();                        // Initialize LCD
+;frecunciometro.c,42 :: 		INTCON =   0xE0;      //habilitando las interrupciones del timer0
+	MOVLW      224
+	MOVWF      INTCON+0
+;frecunciometro.c,43 :: 		TMR1H  = 0xFF;        //registro de los primeros 8 bits del timer1 se llena ya que solo trabajaremos con TMR1L
+	MOVLW      255
+	MOVWF      TMR1H+0
+;frecunciometro.c,44 :: 		TMR1L  = 0x00;        //registro de los segiundos 8 bits del timer1
+	CLRF       TMR1L+0
+;frecunciometro.c,48 :: 		Lcd_Init();                        // Initialize LCD
 	CALL       _Lcd_Init+0
-;frecunciometro.c,53 :: 		Lcd_Cmd(_LCD_CLEAR);               // Clear display
+;frecunciometro.c,49 :: 		Lcd_Cmd(_LCD_CLEAR);               // Clear display
 	MOVLW      1
 	MOVWF      FARG_Lcd_Cmd_out_char+0
 	CALL       _Lcd_Cmd+0
-;frecunciometro.c,54 :: 		Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
+;frecunciometro.c,50 :: 		Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
 	MOVLW      12
 	MOVWF      FARG_Lcd_Cmd_out_char+0
 	CALL       _Lcd_Cmd+0
-;frecunciometro.c,55 :: 		Lcd_Out(2,5,"FRECUENCIA:");                 // Write text in second row
+;frecunciometro.c,51 :: 		Lcd_Out(2,5,"FRECUENCIA:");        // Write text in second row
 	MOVLW      2
 	MOVWF      FARG_Lcd_Out_row+0
 	MOVLW      5
@@ -103,67 +100,31 @@ _main:
 	MOVLW      ?lstr1_frecunciometro+0
 	MOVWF      FARG_Lcd_Out_text+0
 	CALL       _Lcd_Out+0
-;frecunciometro.c,57 :: 		while(1)
+;frecunciometro.c,53 :: 		while(1)
 L_main1:
-;frecunciometro.c,59 :: 		if (RA4_bit==1 && ctrl[1]==0)
-	BTFSS      RA4_bit+0, BitPos(RA4_bit+0)
-	GOTO       L_main5
-	MOVF       _ctrl+1, 0
-	XORLW      0
-	BTFSS      STATUS+0, 2
-	GOTO       L_main5
-L__main10:
-;frecunciometro.c,61 :: 		frecuencia++;
-	INCF       _frecuencia+0, 1
-	BTFSC      STATUS+0, 2
-	INCF       _frecuencia+1, 1
-;frecunciometro.c,62 :: 		RB1_bit=1;
-	BSF        RB1_bit+0, BitPos(RB1_bit+0)
-;frecunciometro.c,63 :: 		ctrl[1]=1;
-	MOVLW      1
-	MOVWF      _ctrl+1
-;frecunciometro.c,64 :: 		}
-L_main5:
-;frecunciometro.c,65 :: 		if (RA4_bit==0){
-	BTFSC      RA4_bit+0, BitPos(RA4_bit+0)
-	GOTO       L_main6
-;frecunciometro.c,66 :: 		ctrl[1]=0;
-	CLRF       _ctrl+1
-;frecunciometro.c,67 :: 		RB1_bit=0;
-	BCF        RB1_bit+0, BitPos(RB1_bit+0)
-;frecunciometro.c,68 :: 		}
-L_main6:
-;frecunciometro.c,71 :: 		if (ctrl[0]==4)
+;frecunciometro.c,62 :: 		if (ctrl==123)
 	MOVF       _ctrl+0, 0
-	XORLW      4
+	XORLW      123
 	BTFSS      STATUS+0, 2
-	GOTO       L_main7
-;frecunciometro.c,73 :: 		ctrl[0]=0;
-	CLRF       _ctrl+0
-;frecunciometro.c,74 :: 		printf=1;
+	GOTO       L_main3
+;frecunciometro.c,64 :: 		printf=1;
 	MOVLW      1
 	MOVWF      _printf+0
-;frecunciometro.c,75 :: 		RB0_bit=0x01;
-	BSF        RB0_bit+0, BitPos(RB0_bit+0)
-;frecunciometro.c,76 :: 		}else {RB0_bit=0x00;}
-	GOTO       L_main8
-L_main7:
-	BCF        RB0_bit+0, BitPos(RB0_bit+0)
-L_main8:
-;frecunciometro.c,77 :: 		if (printf==1)
+;frecunciometro.c,65 :: 		}
+L_main3:
+;frecunciometro.c,66 :: 		if (printf==1)
 	MOVF       _printf+0, 0
 	XORLW      1
 	BTFSS      STATUS+0, 2
-	GOTO       L_main9
-;frecunciometro.c,79 :: 		IntToStr(frecuencia, text);
-	MOVF       _frecuencia+0, 0
+	GOTO       L_main4
+;frecunciometro.c,68 :: 		IntToStr(TMR1L, text); //imprimimos el valor acumulado en TIM1L
+	MOVF       TMR1L+0, 0
 	MOVWF      FARG_IntToStr_input+0
-	MOVF       _frecuencia+1, 0
-	MOVWF      FARG_IntToStr_input+1
+	CLRF       FARG_IntToStr_input+1
 	MOVLW      _text+0
 	MOVWF      FARG_IntToStr_output+0
 	CALL       _IntToStr+0
-;frecunciometro.c,80 :: 		Lcd_Out(3,5,text);
+;frecunciometro.c,69 :: 		Lcd_Out(3,5,text);
 	MOVLW      3
 	MOVWF      FARG_Lcd_Out_row+0
 	MOVLW      5
@@ -171,7 +132,7 @@ L_main8:
 	MOVLW      _text+0
 	MOVWF      FARG_Lcd_Out_text+0
 	CALL       _Lcd_Out+0
-;frecunciometro.c,81 :: 		Lcd_Out_CP("HZ");
+;frecunciometro.c,70 :: 		Lcd_Out_CP("HZ");
 	MOVLW      72
 	MOVWF      ?lstr2_frecunciometro+0
 	MOVLW      90
@@ -180,17 +141,17 @@ L_main8:
 	MOVLW      ?lstr2_frecunciometro+0
 	MOVWF      FARG_Lcd_Out_CP_text+0
 	CALL       _Lcd_Out_CP+0
-;frecunciometro.c,82 :: 		return -1;
-	MOVLW      255
+;frecunciometro.c,71 :: 		return 1;
+	MOVLW      1
 	MOVWF      R0+0
-	MOVLW      255
+	MOVLW      0
 	MOVWF      R0+1
 	GOTO       L_end_main
-;frecunciometro.c,83 :: 		}
-L_main9:
-;frecunciometro.c,84 :: 		}
+;frecunciometro.c,72 :: 		}
+L_main4:
+;frecunciometro.c,74 :: 		}
 	GOTO       L_main1
-;frecunciometro.c,85 :: 		}
+;frecunciometro.c,75 :: 		}
 L_end_main:
 	GOTO       $+0
 ; end of _main
--- frecuenciometro/frecunciometro.cp
+++ frecuenciometro/frecunciometro.cp
@@ -1,37 +1,31 @@
 #line 1 "D:/CODES 2.0/frecuenciometro/frecunciometro.c"
 
 
-sbit LCD_RS at RC4_bit;
-sbit LCD_EN at RC5_bit;
-sbit LCD_D4 at RC0_bit;
-sbit LCD_D5 at RC1_bit;
-sbit LCD_D6 at RC2_bit;
-sbit LCD_D7 at RC3_bit;
+sbit LCD_RS at RD4_bit;
+sbit LCD_EN at RD5_bit;
+sbit LCD_D4 at RD0_bit;
+sbit LCD_D5 at RD1_bit;
+sbit LCD_D6 at RD2_bit;
+sbit LCD_D7 at RD3_bit;
 
-sbit LCD_RS_Direction at TRISC4_bit;
-sbit LCD_EN_Direction at TRISC5_bit;
-sbit LCD_D4_Direction at TRISC0_bit;
-sbit LCD_D5_Direction at TRISC1_bit;
-sbit LCD_D6_Direction at TRISC2_bit;
-sbit LCD_D7_Direction at TRISC3_bit;
-
-
+sbit LCD_RS_Direction at TRISD4_bit;
+sbit LCD_EN_Direction at TRISD5_bit;
+sbit LCD_D4_Direction at TRISD0_bit;
+sbit LCD_D5_Direction at TRISD1_bit;
+sbit LCD_D6_Direction at TRISD2_bit;
+sbit LCD_D7_Direction at TRISD3_bit;
 
 
-unsigned int frecuencia=0;
-unsigned char ctrl[2]={0},printf=0,text[6]={0};
-
+unsigned char ctrl=0,printf=0,text[5]={0};
 
 
 void interrupt(void)
 {
  INTCON.GIE = 0;
- if(INTCON.T0IF == 1)
+ if(INTCON.T0IF == 1 )
  {
  INTCON.T0IF = 0;
- PORTD=~PORTD;
- ctrl[0]++;
-
+ ctrl++;
  }
  INTCON.GIE = 1;
 }
@@ -39,16 +33,18 @@ void interrupt(void)
 
 int main(void)
 {
- TRISB=0x00;
- TRISA = 0xFF;
- PORTA = 0x00;
- ANSELH = 0x00;
- TRISD = 0x00;
- PORTD = 0x00;
- OPTION_REG = 0x27;
- INTCON = 0xA0;
- TMR0 = 0x00;
+
+ TRISC = 0x00;
+ PORTC = 0x00;
+ OPTION_REG = 0x24;
+ T1CON = 0x03;
+ TMR0 = 0;
  OPTION_REG.T0CS = 0;
+ INTCON = 0xE0;
+ TMR1H = 0xFF;
+ TMR1L = 0x00;
+
+
 
  Lcd_Init();
  Lcd_Cmd(_LCD_CLEAR);
@@ -57,30 +53,24 @@ int main(void)
 
  while(1)
  {
- if (RA4_bit==1 && ctrl[1]==0)
- {
- frecuencia++;
- RB1_bit=1;
- ctrl[1]=1;
- }
- if (RA4_bit==0){
- ctrl[1]=0;
- RB1_bit=0;
- }
 
 
- if (ctrl[0]==4)
+
+
+
+
+
+ if (ctrl==123)
  {
- ctrl[0]=0;
  printf=1;
- RB0_bit=0x01;
- }else {RB0_bit=0x00;}
+ }
  if (printf==1)
  {
- IntToStr(frecuencia, text);
+ IntToStr(TMR1L, text);
  Lcd_Out(3,5,text);
  Lcd_Out_CP("HZ");
- return -1;
+ return 1;
  }
+
  }
 }
