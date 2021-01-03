2b2b3f214cb067076900f76996c05d7f49baca1f
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-01-03 16:56:42 -0500

    changes style

--- Sensor de nivel por radiofrecuencia/tele.c
+++ Sensor de nivel por radiofrecuencia/tele.c
@@ -3,17 +3,22 @@
 const char Llenado[]={0b10010000,0b10011000,0b10011100,0b10011110,0b00011111};
 char conteo;
 
-void main() {
-TRISD=0x00;TRISB=0xff; TRISC=0x00;
-PORTD=0x00;PORTB=0x00; PORTC=0x00;
+void main() 
+{
 
-while(1){
-for  (conteo=0;conteo<5;conteo++) {
-if (Start==1){
-SALIDA=Llenado[conteo];
-Delay_ms(3);
-}
-}
-Delay_ms(200);
-}
+  TRISD=0x00;TRISB=0xff; TRISC=0x00;
+  PORTD=0x00;PORTB=0x00; PORTC=0x00;
+  while(1)
+  {
+    for  (conteo=0;conteo<5;conteo++) 
+    {
+      if (Start==1)
+      {
+        SALIDA=Llenado[conteo];
+        Delay_ms(3);
+      }
+    }
+    Delay_ms(200);
+  }
+
 }
